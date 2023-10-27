dcbd87f521d6bb238e377c245addc4f5057940fd
leo <leo.faure@ynov.com>
2023-10-27 11:26:44 +0200

    import project

---
+++ app/src/main/java/com/example/financial_advice/app/settings/settingView.kt
@@ -0,0 +1,18 @@
+package com.example.financial_advice.app.settings
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun widget() {
+    Row () {
+        Text(
+            text = "option 1"
+        )
+        Text(
+            text = ">"
+        )
+    }
+}
